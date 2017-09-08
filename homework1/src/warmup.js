62713c73504de4cff3c71bbfdfddf0cfc11c9c34
Ocean <hzhang2@lion.lmu.edu>
2017-09-07 22:24:35 -0700

    Change program is finished.

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,1 +1,25 @@
 
+// This is will be HW one
+
+function change (amount) {
+  // output an error when the input a negative amount
+  if (amount < 0) {
+    throw new RangeError('amount cannot be negative');
+  }
+  const result = [];
+
+  let remaining = amount;
+
+  [25, 10, 5, 1].forEach((coin) => {
+    result.push(Math.floor(remaining / coin));
+    remaining %=coin;
+  });
+  return result;
+}
+// export this codefor warmup-test.js
+module.exports = {
+  change,
+};
+
+
+function
